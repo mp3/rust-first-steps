7bea7034d52764d2ce9e98dc363a154c6ea47ba4
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-19 21:18:33 +0900

    mutation

--- understand-common-concept/src/main.rs
+++ understand-common-concept/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let a_number = 10;
+    let mut a_number = 10;
     let a_boolean = true;
 
     println!("the number is {}", a_number);
+    a_number = 15;
+    println!("and now the number is {}", a_number);
+
     println!("the boolean is {}", a_boolean);
 }
