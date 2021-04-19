53ee25aba1a5d076edb208c16a905b9daa81dca1
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-19 22:26:23 +0900

    hashmap

--- understand-common-concept/src/main.rs
+++ understand-common-concept/src/main.rs
@@ -1,3 +1,26 @@
+use std::collections::HashMap;
+
 fn main() {
-    
+    let mut book_reviews: HashMap<String, String> = HashMap::new();
+
+    book_reviews.insert(
+        "Adventures of Huckleberry Finn".to_string(),
+        "My favorite book.".to_string(),
+    );
+    book_reviews.insert(
+        "Grimms' Fairy Tales".to_string(),
+        "Masterpiece.".to_string(),
+    );
+    book_reviews.insert(
+        "Pride and Prejudice".to_string(),
+        "Very enjoyable.".to_string(),
+    );
+    book_reviews.insert(
+        "The Adventures of Sherlock Holmes".to_string(),
+        "Eye lyked it alot.".to_string(),
+    );
+
+    if book_reviews.contains_key("Les Misérables") == false {
+        println!("We've got {} reviews, but Les Misérables ain't one.", book_reviews.len());
+    }
 }
