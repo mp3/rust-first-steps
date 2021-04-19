81f2651ea274f1e113b9970d50591632fe1b87ff
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-19 21:19:38 +0900

    shadow

--- understand-common-concept/src/main.rs
+++ understand-common-concept/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     println!("the number is {}", a_number);
     a_number = 15;
     println!("and now the number is {}", a_number);
+    let a_number = a_number * 2;
+    println!("The number is {}", a_number);
 
     println!("the boolean is {}", a_boolean);
 }
