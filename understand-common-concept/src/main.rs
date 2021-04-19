64809d8f8ad1e3df9417e56b3aee0b36a59f2b45
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-19 21:53:55 +0900

    exercise

--- understand-common-concept/src/main.rs
+++ understand-common-concept/src/main.rs
@@ -1,112 +1,39 @@
-fn main() {
-    basic();
-    number();
-    boolean();
-    char_and_string();
-    tuple();
-    instantiate();
-}
-
-fn basic() {
-    let mut a_number = 10;
-    let a_boolean = true;
-
-    println!("the number is {}", a_number);
-    a_number = 15;
-    println!("and now the number is {}", a_number);
-    let a_number = a_number * 2;
-    println!("The number is {}", a_number);
-
-    println!("the boolean is {}", a_boolean);
-
-    let number: u32 = "42".parse().expect("Not a number!");
-    println!("u32 number {}", number);
-}
-
-fn number() {
-    // Number
-    // Addition
-    println!("1 + 2 = {}", 1u32 + 2);
-
-    // Substraction
-    println!("1 - 2 = {}", 1i32 - 2);
-
-    // Interger Division
-    println!("9 / 2 = {}", 9u32 / 2);
-
-    // Float Division
-    println!("9 / 2 = {}", 9.0 / 2.0);
-
-    // Multiplication
-    println!("3 * 6 = {}", 3 * 6);
-}
-
-fn boolean() {
-    // Boolean
-    let is_bigger = 1 > 4;
-    println!("{}", is_bigger);
-}
-
-fn char_and_string() {
-    // Character and String
-    // char
-    let c = 'z';
-    let z = 'Z';
-    let heart_eyes_cat = '😻';
-    println!("{} {} {}", c, z, heart_eyes_cat);
-
-    // String
-    let mut hello = String::from("Hello, ");
-    hello.push('w');
-    hello.push_str("orld!");
-    println!("{}", hello);
-}
-
-fn tuple() {
-    // Tuple
-    let tuple = ("hello", 5, 'c');
-
-    assert_eq!(tuple.0, "hello");
-    assert_eq!(tuple.1, 5);
-    assert_eq!(tuple.2, 'c');
-}
-
-// Struct
-struct Person {
-    name: String,
-    age: u8,
-    likes_oranges: bool
-}
-
-struct Point2D(u32, u32);
-
-struct Unit;
-
-fn instantiate() {
-    let person = Person {
-        name: String::from("Adam"),
-        likes_oranges: true,
-        age: 25
-    };
-
-    let origin = Point2D(0, 0);
-
-    let unit = Unit;
-}
-
-
-// Enum
-enum WebEvent {
-    PageLoad,
-    PageUnload,
-    KeyPress(KeyPress),
-    Paste(String),
-    Click(Click),
-}
-
-struct Click {
-    x: i64,
-    y: i64
-}
-
-struct KeyPress(char);
+struct Car {
+    color: String,
+    transmission: Transmission,
+    convertible: bool,
+    mileage: u32,
+  }
+  
+  #[derive(PartialEq, Debug)]
+  enum Transmission {
+    Manual,
+    SemiAuto,
+    Automatic,
+  }
+  
+  fn car_factory(color: String, transmission: Transmission, convertible: bool) -> Car {
+    let car: Car = todo!("Replace this with an actual Car instance")
+  
+    // Factory's Quality Control Department says that new cars must always have zero mileage!
+    assert_eq!(car.mileage, 0);
+    return car;
+  }
+  
+  fn main() {
+    let client_request_1 = car_factory(String::from("Red"), Transmission::Manual, false);
+    assert_eq!(client_request_1.color, "Red");
+    assert_eq!(client_request_1.transmission, Transmission::Manual);
+    assert_eq!(client_request_1.convertible, false);
+  
+    let client_request_2 = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    assert_eq!(client_request_2.color, "Silver");
+    assert_eq!(client_request_2.transmission, Transmission::Automatic);
+    assert_eq!(client_request_2.convertible, true);
+  
+    let client_request_2 = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    assert_eq!(client_request_2.color, "Yellow");
+    assert_eq!(client_request_2.transmission, Transmission::SemiAuto);
+    assert_eq!(client_request_2.convertible, false);
+  }
+  
---
+++ understand-common-concept/src/study.rs
@@ -0,0 +1,112 @@
+fn main() {
+  basic();
+  number();
+  boolean();
+  char_and_string();
+  tuple();
+  instantiate();
+}
+
+fn basic() {
+  let mut a_number = 10;
+  let a_boolean = true;
+
+  println!("the number is {}", a_number);
+  a_number = 15;
+  println!("and now the number is {}", a_number);
+  let a_number = a_number * 2;
+  println!("The number is {}", a_number);
+
+  println!("the boolean is {}", a_boolean);
+
+  let number: u32 = "42".parse().expect("Not a number!");
+  println!("u32 number {}", number);
+}
+
+fn number() {
+  // Number
+  // Addition
+  println!("1 + 2 = {}", 1u32 + 2);
+
+  // Substraction
+  println!("1 - 2 = {}", 1i32 - 2);
+
+  // Interger Division
+  println!("9 / 2 = {}", 9u32 / 2);
+
+  // Float Division
+  println!("9 / 2 = {}", 9.0 / 2.0);
+
+  // Multiplication
+  println!("3 * 6 = {}", 3 * 6);
+}
+
+fn boolean() {
+  // Boolean
+  let is_bigger = 1 > 4;
+  println!("{}", is_bigger);
+}
+
+fn char_and_string() {
+  // Character and String
+  // char
+  let c = 'z';
+  let z = 'Z';
+  let heart_eyes_cat = '😻';
+  println!("{} {} {}", c, z, heart_eyes_cat);
+
+  // String
+  let mut hello = String::from("Hello, ");
+  hello.push('w');
+  hello.push_str("orld!");
+  println!("{}", hello);
+}
+
+fn tuple() {
+  // Tuple
+  let tuple = ("hello", 5, 'c');
+
+  assert_eq!(tuple.0, "hello");
+  assert_eq!(tuple.1, 5);
+  assert_eq!(tuple.2, 'c');
+}
+
+// Struct
+struct Person {
+  name: String,
+  age: u8,
+  likes_oranges: bool
+}
+
+struct Point2D(u32, u32);
+
+struct Unit;
+
+fn instantiate() {
+  let person = Person {
+      name: String::from("Adam"),
+      likes_oranges: true,
+      age: 25
+  };
+
+  let origin = Point2D(0, 0);
+
+  let unit = Unit;
+}
+
+
+// Enum
+enum WebEvent {
+  PageLoad,
+  PageUnload,
+  KeyPress(KeyPress),
+  Paste(String),
+  Click(Click),
+}
+
+struct Click {
+  x: i64,
+  y: i64
+}
+
+struct KeyPress(char);
