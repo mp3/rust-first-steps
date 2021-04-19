52817fd644e834e1cf9c65c35df1a24e37070750
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-19 23:00:30 +0900

    match

--- understand-common-concept/src/main.rs
+++ understand-common-concept/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    panic!("Farewell!");
+    let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
+
+    for &index in [0, 2, 99].iter() {
+        match fruits.get(index) {
+            Some(&"coconut") => println!("Coconuts are awesome!!!"),
+            Some(fruit_name) => println!("It's a delicious {}!", fruit_name),
+            None => println!("There is no fruit! :("),
+        }
+    }
 }
