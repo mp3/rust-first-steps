cf90ece6137bcdf823f670d7a0626175cf991626
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-19 21:17:11 +0900

    variables

--- understand-common-concept/src/main.rs
+++ understand-common-concept/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let a_number = 10;
+    let a_boolean = true;
+
+    println!("the number is {}", a_number);
+    println!("the boolean is {}", a_boolean);
 }
