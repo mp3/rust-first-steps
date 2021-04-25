cd13dfd656020707c63c749027db1c4610fdffff
mp3 <im.in.a.coma.xxx@gmail.com>
2021-04-25 22:08:55 +0900

    define struct

--- rusty-journal/src/cli.rs
+++ rusty-journal/src/cli.rs
@@ -1,1 +1,12 @@
-mod cli;
+use std::path::PathBuf;
+
+pub enum Action {
+  Add { task: String },
+  Done { position: usize },
+  List,
+}
+
+pub struct CommandLineArgs {
+  pub action: Action,
+  pub journal_file: Option<PathBuf>,
+}
